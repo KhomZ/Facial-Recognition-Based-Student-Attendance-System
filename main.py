0712037c4b86add9f9f452ddd15ce24c1033b4a6
ikhom <mgrprinc37@gmail.com>
2021-04-06 19:39:19 +0545

    added main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# Developer : team kyzen
+# Date : 
+
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+
+class Face_Recognition_System:
+    def __init__(self, root):
+        self.root = root
+        self.root.geometry("1530x790+0+0")
+        self.root.title("Face Recognition System")
+
+        img = Image.open()
+
+
+
+if __name__ == "__main__":
+    root = Tk()
+    obj = Face_Recognition_System(root)
+    root.mainloop()
+
